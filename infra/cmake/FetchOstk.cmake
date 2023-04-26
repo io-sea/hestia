53b44c1e98605c20ca014e1fccf9db3d1036311c
Caelen Feller <caelen.feller@ichec.ie>
2023-04-26 11:57:42 +0100

    Remove hestia_build_openssl macro

--- infra/cmake/FetchOstk.cmake
+++ infra/cmake/FetchOstk.cmake
@@ -1,26 +1,5 @@
 include(FetchContent)
 
-macro(hestia_build_openssl)
-    ExternalProject_Add(openssl
-    URL https://www.openssl.org/source/openssl-1.1.1t.tar.gz
-    URL_HASH          SHA256=8dee9b24bdb1dcbf0c3d1e9b02fb8f6bf22165e807f45adeb7c9677536859d3b
-    CONFIGURE_COMMAND <SOURCE_DIR>/config --prefix=<INSTALL_DIR> --libdir=lib
-    INSTALL_COMMAND make install_sw
-    )
-
-    set(OPENSSL_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl-prefix)
-    file(MAKE_DIRECTORY ${OPENSSL_INSTALL_DIR}/include)
-    add_library(OpenSSL::SSL STATIC IMPORTED GLOBAL)
-    set_property(TARGET OpenSSL::SSL PROPERTY IMPORTED_LOCATION ${OPENSSL_INSTALL_DIR}/lib/libssl.a)
-    set_property(TARGET OpenSSL::SSL PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INSTALL_DIR}/include)
-    add_dependencies(OpenSSL::SSL openssl)
-
-    add_library(OpenSSL::Crypto STATIC IMPORTED GLOBAL)
-    set_property(TARGET OpenSSL::Crypto PROPERTY IMPORTED_LOCATION ${OPENSSL_INSTALL_DIR}/lib/libcrypto.a)
-    set_property(TARGET OpenSSL::Crypto PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INSTALL_DIR}/include)
-    add_dependencies(OpenSSL::Crypto openssl)
-endmacro()
-
 macro(hestia_fetch_openssl)
     find_package(OpenSSL 1.1.1 QUIET)
     if (NOT ${OpenSSL_FOUND})
@@ -28,9 +7,6 @@ macro(hestia_fetch_openssl)
             set(OPENSSL_ROOT_DIR /opt/homebrew/opt/openssl@1.1)
             find_package(OpenSSL 1.1.1 QUIET)
         endif()
-    endif()
-    if (NOT ${OpenSSL_FOUND})
-        hestia_build_openssl()
     endif()
 endmacro()
 
