3cc593d914d7352a7e390bcc80ccf4a54dbad20f
buketbenek <buket.gursoy@ichec.ie>
2023-07-29 20:13:54 +0100

    JsonUtils has/remove key/keys

--- test/unit_tests/base/common/TestJsonUtils.cc
+++ test/unit_tests/base/common/TestJsonUtils.cc
@@ -1,10 +1,36 @@
 #include <catch2/catch_all.hpp>
 
 #include "JsonUtils.h"
+#include "TestUtils.h"
 
+#include <fstream>
 #include <iostream>
 
-TEST_CASE("Test JsonUtils - get values", "[common]")
+TEST_CASE("Test JsonUtils - read/write", "[common]")
+{
+    auto test_output_dir = TestUtils::get_test_output_dir();
+    auto json_file_path   = test_output_dir / "json_file.txt";
+
+    std::ofstream json_file;
+    json_file.open(json_file_path.string(), std::ios::trunc);  // Clear file
+
+    hestia::Map metadata;
+    metadata.set_item("my_key0", "my_val0");
+    metadata.set_item("my_key1", "my_val1");
+    hestia::JsonUtils::write(json_file_path, metadata, 0);
+    metadata.set_item("my_key2", "my_val2");
+    hestia::JsonUtils::write(json_file_path, metadata, 1);
+
+    hestia::Map expected_metadata;
+    hestia::JsonUtils::read(json_file_path, expected_metadata, {"my_key0", "my_key1", "my_key2"});
+    REQUIRE(expected_metadata.get_item("my_key0") == "my_val0");
+    REQUIRE(expected_metadata.get_item("my_key1") == "my_val1");
+    REQUIRE(expected_metadata.get_item("my_key2") == "my_val2");
+
+    json_file.close();
+}
+
+TEST_CASE("Test JsonUtils - set/get values", "[common]")
 {
     std::string input =
         "{\"my_key0\":\"my_val0\", \"my_key1\":\"my_val1\", \"my_key2\":\"my_val2\"}";
@@ -14,6 +40,68 @@ TEST_CASE("Test JsonUtils - get values", "[common]")
     REQUIRE(values.size() == 2);
     REQUIRE(values[0] == "my_val0");
     REQUIRE(values[1] == "my_val1");
+
+    auto test_output_dir = TestUtils::get_test_output_dir();
+    auto json_file_path   = test_output_dir / "json_file.txt";
+    std::ofstream json_file;
+    json_file.open(json_file_path.string(), std::ios::trunc);  // Clear file
+    hestia::Map metadata;
+    metadata.set_item("my_key0", "my_val0");
+    hestia::JsonUtils::write(json_file_path, metadata, 0);
+    
+    std::vector<hestia::KeyValuePair> kv_pairs;
+    kv_pairs={{"my_key1", "my_val1"}, {"my_key2", "my_val2"}};
+    //hestia::JsonUtils::set_values(json_file_path, {{"my_key1", "my_val1"}, {"my_key2", "my_val2"}});
+
+    std::vector<std::string> given_keys = {"my_key0"};
+    std::vector<std::string> expected_values;
+    hestia::JsonUtils::get_values(json_file_path, given_keys, expected_values);
+    //std::cout<<expected_values[0]<<std::endl;
+    //REQUIRE(expected_values[0] == "my_val0");
+
+    //hestia::JsonUtils::set_value(json_file_path, "my_key3", "my_val3");
+
+    std::string given_key = {"my_key0"};
+    std::string expected_value;
+    bool value=hestia::JsonUtils::get_value(json_file_path, given_key, expected_value);
+    //std::cout<<value << expected_value<<std::endl;
+    REQUIRE(value == 1);
+    REQUIRE(expected_value == "my_val0");
+
+    json_file.close();
+}
+
+TEST_CASE("Test JsonUtils - has/remove keys", "[common]")
+{
+    auto test_output_dir = TestUtils::get_test_output_dir();
+    auto json_file_path   = test_output_dir / "json_file.txt";
+    std::ofstream json_file;
+    json_file.open(json_file_path.string(), std::ios::trunc);  // Clear file
+    hestia::Map metadata;
+    metadata.set_item("my_key0", "my_val0");
+    metadata.set_item("my_key1", "my_val1");
+    metadata.set_item("my_key2", "my_val2");
+    hestia::JsonUtils::write(json_file_path, metadata, 0);
+
+    bool value_found=hestia::JsonUtils::has_key(json_file_path, {"my_key0"});
+    REQUIRE(value_found == 1);
+
+    std::vector<bool> expected_found;
+    hestia::JsonUtils::has_keys(json_file_path, {"my_key0", "my_key1"}, expected_found);
+    REQUIRE(expected_found[0] == 1);
+    REQUIRE(expected_found[1] == 1);
+
+    hestia::JsonUtils::remove_key(json_file_path, {"my_key0"});
+    value_found=hestia::JsonUtils::has_key(json_file_path, {"my_key0"});
+    REQUIRE(value_found == 0);
+
+    std::vector<bool> expected_founds;
+    hestia::JsonUtils::remove_keys(json_file_path, {"my_key1", "my_key2"});
+    hestia::JsonUtils::has_keys(json_file_path, {"my_key1", "my_key2"}, expected_founds);
+    REQUIRE(expected_founds[0] == 0);
+    REQUIRE(expected_founds[1] == 0);
+
+    json_file.close();
 }
 
 TEST_CASE("Test JsonUtils - metadata to and from json", "[common]")
@@ -22,14 +110,26 @@ TEST_CASE("Test JsonUtils - metadata to and from json", "[common]")
     metadata.set_item("my_key0", "my_val0");
     metadata.set_item("my_key1", "my_val1");
     metadata.set_item("my_key2", "my_val2");
-
     const auto as_json = hestia::JsonUtils::to_json(metadata);
 
     hestia::Map expected_metadata;
     hestia::JsonUtils::from_json(as_json, expected_metadata);
-
     REQUIRE(expected_metadata.get_item("my_key0") == "my_val0");
     REQUIRE(expected_metadata.get_item("my_key1") == "my_val1");
+
+    std::vector<hestia::Map> metadata_vector;
+    metadata_vector.push_back(expected_metadata);
+    for (const auto& md : metadata_vector) {
+        std::cout<<md<<std::endl;
+    }
+    const auto as_json_vector = hestia::JsonUtils::to_json(metadata_vector, "KEY0");
+    std::cout<<as_json_vector<<std::endl; 
+
+    //REQUIRE(expected_metadata.get_item("KEY0") == metadata_vector[0]);
+
+    //hestia::Map expected_metadata_vector;
+    //hestia::JsonUtils::from_json(as_json_vector, expected_metadata_vector);
+    
 }
 
 TEST_CASE("Test JsonUtils - dict to and from json", "[common]")
