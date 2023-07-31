72a6f5dfcded18662dddb5f12bb42c2c4539c3cc
jgrogan <james.grogan@ichec.ie>
2023-07-31 14:03:57 +0100

    Merge hsm object store into base layer.

--- src/base/storage/requests/HsmObjectStoreResponse.cc
+++ src/base/storage/requests/HsmObjectStoreResponse.cc
@@ -9,14 +9,6 @@ HsmObjectStoreResponse::HsmObjectStoreResponse(
 
 HsmObjectStoreResponse::HsmObjectStoreResponse(
     const BaseObjectStoreRequest& request,
-    HsmMiddlewareResponse::Ptr middleware_response) :
-    BaseObjectStoreResponse<HsmObjectStoreErrorCode>(request),
-    m_middleware_response(std::move(middleware_response))
-{
-}
-
-HsmObjectStoreResponse::HsmObjectStoreResponse(
-    const BaseObjectStoreRequest& request,
     HsmObjectStoreResponse::Ptr hsm_child_response) :
     BaseObjectStoreResponse<HsmObjectStoreErrorCode>(request)
 {
@@ -35,14 +27,6 @@ HsmObjectStoreResponse::Ptr HsmObjectStoreResponse::create(
     const BaseObjectStoreRequest& request)
 {
     return std::make_unique<HsmObjectStoreResponse>(request);
-}
-
-HsmObjectStoreResponse::Ptr HsmObjectStoreResponse::create(
-    const BaseObjectStoreRequest& request,
-    HsmMiddlewareResponse::Ptr middleware_response)
-{
-    return std::make_unique<HsmObjectStoreResponse>(
-        request, std::move(middleware_response));
 }
 
 HsmObjectStoreResponse::Ptr HsmObjectStoreResponse::create(
