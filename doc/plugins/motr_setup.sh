9e7cc7934f129bb6453ee6ef14299e4eea6d533f
oconnor <oconnor@localhost.localdomain>
2024-03-11 13:19:50 -0400

    Start yaml file auto generation

--- doc/plugins/motr_setup.sh
+++ doc/plugins/motr_setup.sh
@@ -12,6 +12,10 @@ export CLIENT_HA_ADDR=$(hctl status | awk '/hax/ {print $4}')
 export CLIENT_PROFILE=$(hctl status | awk '/default/ {print $1}')
 export CLIENT_PROC_FID=$(hctl status | awk '/m0_client/ {print $3}' | head -n 1)
 export CLIENT_LADDR=$(hctl status | awk '/m0_client/ {print $4}' | head -n 1)
+# add angle brakcets to the ones of these that need them
 
 m0composite "$CLIENT_LADDR" "$CLIENT_HA_ADDR" "$CLIENT_PROFILE" "$CLIENT_PROC_FID"
+
+hctl status | sed '1,/Profile/!d' | awk '/pool/ {print}' | tail -n +2
+
 
