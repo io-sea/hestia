143fa1b587cde0ce053cd7ae0b60a173e265d099
oconnor <oconnor@localhost.localdomain>
2024-03-12 08:43:47 -0400

    Add pools to setup script

--- infra/scripts/motr_setup.sh
+++ infra/scripts/motr_setup.sh
@@ -10,12 +10,24 @@ sudo hctl bootstrap --mkfs ~/singlenode-multipools.yaml
 
 export CLIENT_HA_ADDR=$(hctl status | awk '/hax/ {print $4}')
 export CLIENT_PROFILE=$(hctl status | awk '/default/ {print $1}')
+export CLIENT_PROFILE="<$CLIENT_PROFILE>"
 export CLIENT_PROC_FID=$(hctl status | awk '/m0_client/ {print $3}' | head -n 1)
+export CLIENT_PROC_FID="<$CLIENT_PROC_FID>"
 export CLIENT_LADDR=$(hctl status | awk '/m0_client/ {print $4}' | head -n 1)
 # add angle brakcets to the ones of these that need them
 
 m0composite "$CLIENT_LADDR" "$CLIENT_HA_ADDR" "$CLIENT_PROFILE" "$CLIENT_PROC_FID"
 
-hctl status | sed '1,/Profile/!d' | awk '/pool/ {print}' | tail -n +2
+export POOLS=$(hctl status | sed '1,/Profile/!d' | awk '/pool/ {print}' | tail -n +2 | awk '{print $1}')
+export NUM_POOLS=$(echo $POOLS | wc -w)
 
+echo "Add the following under the config section of the motr yaml file:"
+
+export i=1
+for pool in $POOLS
+do
+#       export pool$i=$($POOLS | awk '{print $i}')
+        echo M0_POOL_TIER$i=$pool
+        ((i++))
+done
 
