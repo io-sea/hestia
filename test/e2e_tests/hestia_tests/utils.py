c0ebbdc75b548f13855e41993e15f64170ded37d
Katie O'Connor <katie.oconnor@ichec.ie>
2024-04-03 09:07:26 +0100

    Remove extra function call with logging

--- test/e2e_tests/hestia_tests/utils.py
+++ test/e2e_tests/hestia_tests/utils.py
@@ -1,6 +1,6 @@
 import logging
 
-def setup_default_logging(work_dir, log_to_console):
+def setup_default_logging(work_dir):
     logging.basicConfig(filename=work_dir, filemode='w', 
             format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
             level=logging.INFO)
